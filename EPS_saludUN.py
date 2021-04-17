aab0208bbef175aee8f51c102a36bed9bea17a9b
dalpulidodu <dalpulidodu@unal.edu.co>
2021-04-17 13:27:10 -0500

    Funciones que crea una base de datos
    Crear cualquier base de datos sqlite3

--- EPS_saludUN.py
+++ EPS_saludUN.py
@@ -1,1 +1,7 @@
-
+def sql_connection():
+    """Funcion que crea la conexion en la Base de datos Sqlite3 """
+    try:
+        con = sqlite3.connect('BDSQlLiteEjercicioClase.db')
+        return con
+    except Error:
+        print(Error)
