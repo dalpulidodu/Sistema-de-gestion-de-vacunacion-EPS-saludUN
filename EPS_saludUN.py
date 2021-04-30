336bbe6b92f45e040f862071255ffd7062b589e7
dalpulidodu <dalpulidodu@unal.edu.co>
2021-04-30 17:03:48 -0500

    menus graficos vacunas
    se agregan funciones que pintan las opciones de fabricantes y tipos de vacunas
    se corrige la funcion read_date() para recibir las fechas de vencimiento de vacunas correctamente

--- EPS_saludUN.py
+++ EPS_saludUN.py
@@ -602,7 +602,43 @@ def menu_calendar_vaccune():
     Programacion de citas de vacunacion
     
     ''')
+
+def menu_factory():
+    """
+    Imprime el menu de seleccion de fabricas
+
+    Returns
+    -------
+    None.
+
+    """
+    print('''  Fabricas:
+        1. Sinovak 
+        2. Pfizer 
+        3. Moderna 
+        4. Sputnik V 
+        5. AstraZeneca 
+        6. Sinopharm 
+        7. Covaxim 
     
+    ''')   
+    
+def menu_type_vaccumm():
+    """
+    Imprime el menu de seleccion de tipo de vacunas
+
+    Returns
+    -------
+    None.
+
+    """
+    print('''  Tipo de vacunas:
+        1. Vector viral
+        2. ARN/ADN
+        3. Virus desactivado
+        4. En base a proteínas
+    
+    ''')    
 ##########################################################################################################
 #                                        Bussisnes logic
 ##########################################################################################################
@@ -668,7 +704,10 @@ def read_date(word):
 
     Imprime un encabezado con el tipo de fecha que solicita, y pide al usuario 
     ingresar el dia, mes y año. Comprobando que se ingresen datos numericos. 
-    Retorna un string con el frmato de fecha  DD/MM/AAAA
+    Si word es 'es vacunacion' valida que las entradas de fechas sean mayores 
+    a la fecha en la que se hace la solicitud. De lo contrario obliga a que la 
+    fecha ingresadas sea menor que la fecha en la que se hace la solicitud.
+    Retorna un string con el formato de fecha  DD/MM/AAAA
     
     Parameters
     ----------
@@ -683,6 +722,7 @@ def read_date(word):
     date_aux: String fecha
 
     """
+    
     correct_date = False
     while not correct_date :
         print()
@@ -731,13 +771,18 @@ def read_date(word):
                   
         date_aux =ano+"-"+mes+"-"+dia
         
-   
-        if date_aux == '0000-00-00':
-            correct_date = True
-        elif date.fromisoformat(date_aux) > date.today():
-            print('Fecha fuera de rango, intentelo de nuevo.')
-        else:
-            correct_date = True
+        if (word =='de vencimiento'):
+            if date.fromisoformat(date_aux) < date.today():
+                print('Fecha fuera de rango, intentelo de nuevo.')
+            else:
+                correct_date = True
+        else:    
+            if date_aux == '0000-00-00':
+                correct_date = True
+            elif date.fromisoformat(date_aux) > date.today():
+                print('Fecha fuera de rango, intentelo de nuevo.')
+            else:
+                correct_date = True
             
     date_aux =dia+"/"+mes+"/"+ano          
     return date_aux
